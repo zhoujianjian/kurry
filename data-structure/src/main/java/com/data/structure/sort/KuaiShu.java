3db0860b2cfe235dea6d04665c9101336b905722
zhoujian <zhoujian@youanmi.com>
2020-05-26 15:48:42 +0800

    add

--- data-structure/src/main/java/com/data/structure/sort/KuaiShu.java
+++ data-structure/src/main/java/com/data/structure/sort/KuaiShu.java
@@ -1,4 +1,4 @@
-package com.data.structure;
+package com.data.structure.sort;
 
 /**
  * @ClassName: kuaishu
--- data-structure/src/main/java/com/data/structure/sort/MaoPao.java
+++ data-structure/src/main/java/com/data/structure/sort/MaoPao.java
@@ -1,4 +1,4 @@
-package com.data.structure;
+package com.data.structure.sort;
 
 import java.util.Arrays;
 
