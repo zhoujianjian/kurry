7ee402df09a1937700d1358b213c3b47d12656da
zhou <quncaozhoujian@gamil.com>
2021-07-28 14:10:01 +0800

    快速排序test

--- data-structure/src/main/java/com/data/structure/sort/KuaiShu.java
+++ data-structure/src/main/java/com/data/structure/sort/KuaiShu.java
@@ -1,5 +1,9 @@
 package com.data.structure.sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @ClassName: kuaishu
  * @Description: 描述
@@ -12,13 +16,41 @@ public class KuaiShu {
 
     public static void main(String[] args) {
         int[] array =new int[]{6,5,8,2,3,9,4,10};
+        quickSort(array,0,array.length-1);
+        System.out.println(Arrays.toString(array));
+    }
+
+
+    public static void quickSort( int[] array ,int start ,int end ){
+        if ( start>= end){
+          return;
+        }
+        //基准
+        int base = array[start];
+        //左边
+        int low = start;
+        int high = end;
+
+        while (low < high ){
+            while (low < high && base<=array[high]){
+                high --;
+            }
+            array[low] = array[high];
+
+            //最低的位置比 基准数 大
+            while (low < high && base >= array[low]){
+                low ++;
+            }
+            array[high] = array[low];
+        }
+        array[low]=high;
+
+        quickSort(array,start,low);
+        quickSort(array,low+1,end);
 
     }
 
 
-    public static void kuaipai( int[] array ){
-   ///
 
-    }
 
 }
